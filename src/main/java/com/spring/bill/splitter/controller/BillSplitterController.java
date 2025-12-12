aa83274c63bc9b161a5b85b6c4359801e11a589f
shukrulloHakimjanov <hakimjanovshukrullo0@gmail.com>
2025-12-12 14:12:05 +0500

    fix error in controller

--- src/main/java/com/spring/bill/splitter/controller/BillSplitterController.java
+++ src/main/java/com/spring/bill/splitter/controller/BillSplitterController.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @AllArgsConstructor
+@Validated
 @RequestMapping("/api/v1/bill")
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class BillSplitterController {
